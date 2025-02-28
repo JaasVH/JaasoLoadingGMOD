1357c690589b544d7c1687cac9a869dae7931c3a
Jaaso <72317334+JaasVH@users.noreply.github.com>
2025-02-28 00:04:39 -0300

    upload it right, am i dumb?
    lol

---
+++ js/config.js
@@ -0,0 +1,51 @@
+// ignore
+var Config = {};
+
+/**
+ * What should the text in the center of the screen be?
+ * if empty it will fill in your Server Name
+ */
+Config.title = "Bienvenido a JaasoGMOD :D";
+
+/**
+ * Enable map text in the top left corner of the screen?
+ */
+Config.enableMap = true;
+
+/**
+ * Enable steamId text in the top right corner of the screen?
+ */
+Config.enableSteamID = true;
+
+/**
+ * Enable announcements?
+ */
+Config.enableAnnouncements = true;
+
+/**
+ * What messages do you want to show up?
+ * only works if enableAnnouncements = true
+ */
+Config.announceMessages = [
+  "Disfruta del servidor!",
+  "Elian es un boluuuu, ahhhhh",
+  "Que ondaaaap",
+  "JaasoGMOD best server"
+];
+
+/**
+ * How many miliseconds for each announcement?
+ * only works if enableAnnouncements = true
+ */
+Config.announcementLength = 3000;
+
+/**
+ * Image Filename
+ * DROP IMAGE IN "images" FOLDER
+ */
+Config.backgroundImage = "";
+
+/**
+ * Enable debug messages?
+ */
+Config.enableDebug = false;
